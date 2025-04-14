de97b2ec3c11dcbfe3ae0c27456eb16fe92e047a
Valeriya Petkova <valeriya-petkova@abv.bg>
2025-04-14 21:30:07 +0300

    Update routes

--- educationalPlatform/server/src/routes.js
+++ educationalPlatform/server/src/routes.js
@@ -1,29 +1,27 @@
 const router = require('express').Router();
 
-// const courseController = require('./controllers/courseController');
-// const commentController = require('./controllers/commentController');
-// const topicController = require('./controllers/topicController');
-// const participantController = require('./controllers/participantController');
+const courseController = require('./controllers/courseController');
+const commentController = require('./controllers/commentController');
+const topicController = require('./controllers/topicController');
+const participantController = require('./controllers/participantController');
 const userController = require('./controllers/userController');
-// const { attachCourseId } = require('./middlewares/attachCourseIdMiddleware');
-// const { isAuth } = require('./middlewares/authMiddleware');
+const { attachCourseId } = require('./middlewares/attachCourseIdMiddleware');
+const { isAuth } = require('./middlewares/authMiddleware');
 
 
 router.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-//router.use(homeController);
-// router.use('/courses', courseController);
-// router.use(`/courses/:courseId/comments`, (req, res, next) => {
-//     const courseId = req.params.courseId;
-//     req.courseId = courseId;
-//     console.log('courseId', courseId);
-//     next()
-// }, commentController);
+router.use('/courses', courseController);
+router.use(`/courses/:courseId/comments`, (req, res, next) => {
+    const courseId = req.params.courseId;
+    req.courseId = courseId;
+    next()
+}, commentController);
 
-// router.use(`/courses/:courseId/topics`, attachCourseId, topicController);
-// router.use(`/courses/:courseId/participants`, isAuth, attachCourseId, participantController);
+router.use(`/courses/:courseId/topics`, attachCourseId, topicController);
+router.use(`/courses/:courseId/participants`, isAuth, attachCourseId, participantController);
 
 router.use('/users', userController);
 
