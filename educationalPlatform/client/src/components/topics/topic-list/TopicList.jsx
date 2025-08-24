e61cfea751831d3467b96c5128fdce3aaf8d9b17
Valeriya Petkova <valeriya-petkova@abv.bg>
2025-08-24 18:19:24 +0300

    Create Topic List component

---
+++ educationalPlatform/client/src/components/topics/topic-list/TopicList.jsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect, useState } from 'react';
+
+import * as topicService from '../../../services/topicService';
+// import TopicItem from '../topic-item/TopicItem';
+// import TopicCreate from '../topic-create/TopicCreate';
+import { useParams } from 'react-router-dom';
+
+import AuthContext from '../../../contexts/authContext';
+
+export default function TopicList() {
+    const [topics, setTopics] = useState([]);
+    const { courseId } = useParams();
+    const { isAuthenticated, role } = useContext(AuthContext);
+
+    useEffect(() => {
+        topicService.getAll(courseId)
+            .then(result => {
+                setTopics(result)
+            })
+            .catch(error => {
+                console.log(error); 
+            });
+    }, [courseId]);
+    
+    return (
+        <div>
+            <h2>Topics</h2>
+
+            {/* {topics.map(topic => (
+                <TopicItem key={topic._id} {...topic} />
+            ))} */}
+
+            {topics.length === 0 && (
+                <h3>No topics yet</h3>
+            )}
+
+            {/* <TopicCreate /> */}
+        </div>
+    );
+}
